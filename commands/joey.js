edaf462ef42bef0201f0d3a80a37679a1502a44e
Eric Kong <eric.kong98@gmail.com>
2020-06-26 12:23:59 -0400

    joey cmd - add embed for nicer voting UX

--- commands/joey.js
+++ commands/joey.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 // const {
 //   eric,
 //   joey,
@@ -23,38 +25,47 @@ module.exports = {
     if (message.author.id === francis) {
       message.reply("Sorry, you do not have access to that command.");
     } else {
-      const filter = (reaction, user) => reaction.emoji.name === "👍";
-      const collector = message.createReactionCollector(filter, {
-        time: 10000
-      });
-      let num_votes = 0;
-      collector.on("collect", response => {
-        console.log(`Collected ${response.emoji.name}`);
-        num_votes += 1;
-      });
+      const exampleEmbed = new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle("Vote to Move Joey")
+        .setDescription("React to this to message to cast your vote");
+      message.channel.send(exampleEmbed).then(new_msg => {
+        const filter = (reaction, user) => reaction.emoji.name === "👍";
+        const collector = new_msg.createReactionCollector(filter, {
+          time: 10000
+        });
+        let num_votes = -1;
+        collector.on("collect", response => {
+          console.log(`Collected ${response.emoji.name}`);
+          num_votes += 1;
+        });
+        new_msg.react("👍");
+        collector.on("end", collected => {
+          console.log(`Collected ${num_votes} votes`);
+          const voiceStateCache = message.guild.voiceStates.cache;
+          let num_users = voiceStateCache.size;
 
-      collector.on("end", collected => {
-        console.log(`Collected ${num_votes} votes`);
-        const voiceStateCache = message.guild.voiceStates.cache;
-        let num_users = voiceStateCache.size;
-
-        if (num_votes >= num_users / 2) {
-          if (message.channel.guild.name === name_test) {
-            const voice_state = voiceStateCache.get(eric);
-            const guild = message.channel.guild;
-            const move_to_channel = guild.channels.cache.get(jail);
-            voice_state.setChannel(move_to_channel);
+          if (num_votes >= num_users / 2) {
+            if (message.channel.guild.name === name_test) {
+              const voice_state = voiceStateCache.get(eric);
+              const guild = message.channel.guild;
+              const move_to_channel = guild.channels.cache.get(jail);
+              voice_state.setChannel(move_to_channel);
+              message.channel.send("Test-Eric has been moved.");
+            } else {
+              const voice_state = voiceStateCache.get(joey);
+              const guild = message.channel.guild;
+              const move_to_channel = guild.channels.cache.get(afk_bench);
+              voice_state.setChannel(move_to_channel);
+              message.channel.send("Joey has been moved.");
+            }
           } else {
-            const voice_state = voiceStateCache.get(joey);
-            const guild = message.channel.guild;
-            const move_to_channel = guild.channels.cache.get(afk_bench);
-            voice_state.setChannel(move_to_channel);
-            message.channel.send("Joey has been moved.");
+            message.channel.send("Not enough votes to move Joey.");
           }
-        } else {
-          message.channel.send("Not enough votes to move Joey.");
-        }
+        });
       });
+
+      message.delete();
     }
   }
 };
