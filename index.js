fed451faf9013390de788429d27eb6fe175e9c1d
Eric Kong <eric.kong98@gmail.com>
2020-07-08 15:10:43 -0400

    more voice commands

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
-// const { prefix, token, juan } = require("./config.json");
+// const { prefix, token, juan, donge } = require("./config.json");
 const prefix = process.env.prefix;
 const token = process.env.token;
 const juan = process.env.juan;
+const donge = process.env.donge;
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -36,6 +37,34 @@ client.on("message", message => {
   } catch (error) {
     console.error(error);
     message.reply("there was an error trying to execute that command!");
+  }
+});
+
+client.on("voiceStateUpdate", async (oldState, newState) => {
+  if (oldState.id === donge && newState.id === donge) {
+    if (!oldState.channelID && newState.channelID) {
+      const connection = await client.channels.cache
+        .get(newState.channelID)
+        .join();
+      const dispatcher = connection.play("./assets/donger-sigh.mp3");
+      dispatcher.setVolume(0.8);
+      dispatcher.on("start", () => {
+        console.log("Audio is now playing!");
+      });
+
+      dispatcher.on("finish", () => {
+        console.log("Audio has finished playing!");
+        dispatcher.destroy();
+        connection.disconnect();
+      });
+
+      // Always remember to handle errors appropriately!
+      dispatcher.on("error", err => {
+        console.log(err);
+        dispatcher.destroy();
+        connection.disconnect();
+      });
+    }
   }
 });
 
