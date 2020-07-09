adabea97fddfbd56924867137b86d306f5dc1804
Jake <jakeshin22@gmail.com>
2020-07-09 14:33:15 -0400

    Tomo arigato Mr. Roboto

---
+++ commands/sukmydiq.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "suckit",
+  description: "Suq my diq",
+  usage: "",
+  execute(message, args) {
+    message.channel.send("Suq my diq Robert!", {
+      files: [
+        {
+          attachment: "./assets/robface.jpg",
+          name: "robface.jpg"
+        }
+      ]
+    });
+  }
+};
