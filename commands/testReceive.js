f8d1c75347590adbef0ae32ac9c30def8801aed7
Eric Kong <eric.kong98@gmail.com>
2020-07-09 18:32:16 -0400

    test

--- commands/testReceive.js
+++ commands/testReceive.js
@@ -1,45 +1,45 @@
-const fs = require("fs");
-var FileWriter = require("wav").FileWriter;
-
-module.exports = {
-  name: "listen",
-  description: "Juan Is Always Watching :wink:",
-  usage: "mention another user to stalk them!",
-  cooldown: 5,
-  async execute(message, args) {
-    if (message.member.voice.channel) {
-      const target_user = message.mentions.members.first();
-      if (!target_user) {
-        message.channel.send("Error: Please mention a user.");
-      } else {
-        const username = target_user.user.username;
-        const connection = await message.member.voice.channel.join();
-
-        // connection.on("disconnect", () => {
-        //   fs.unlink("./" + username + "_voice.wav", err => {
-        //     if (err) throw err;
-        //     console.log("Recording was deleted");
-        //   });
-        // });
-        const audio = connection.receiver.createStream(target_user, {
-          mode: "pcm"
-        });
-        let writeStream = new FileWriter("./voice.wav", {
-          sampleRate: 48000,
-          channels: 2
-        });
-        audio.pipe(writeStream);
-        audio.resume();
-
-        audio.on("end", () => {
-          message.channel.send("Clip of " + username, {
-            files: ["./voice.wav"]
-          });
-          connection.disconnect();
-        });
-      }
-    } else {
-      message.channel.send("Error: You must be in a voice channel.");
-    }
-  }
-};
+// const fs = require("fs");
+// var FileWriter = require("wav").FileWriter;
+//
+// module.exports = {
+//   name: "listen",
+//   description: "Juan Is Always Watching :wink:",
+//   usage: "mention another user to stalk them!",
+//   cooldown: 5,
+//   async execute(message, args) {
+//     if (message.member.voice.channel) {
+//       const target_user = message.mentions.members.first();
+//       if (!target_user) {
+//         message.channel.send("Error: Please mention a user.");
+//       } else {
+//         const username = target_user.user.username;
+//         const connection = await message.member.voice.channel.join();
+//
+//         // connection.on("disconnect", () => {
+//         //   fs.unlink("./" + username + "_voice.wav", err => {
+//         //     if (err) throw err;
+//         //     console.log("Recording was deleted");
+//         //   });
+//         // });
+//         const audio = connection.receiver.createStream(target_user, {
+//           mode: "pcm"
+//         });
+//         let writeStream = new FileWriter("./voice.wav", {
+//           sampleRate: 48000,
+//           channels: 2
+//         });
+//         audio.pipe(writeStream);
+//         audio.resume();
+//
+//         audio.on("end", () => {
+//           message.channel.send("Clip of " + username, {
+//             files: ["./voice.wav"]
+//           });
+//           connection.disconnect();
+//         });
+//       }
+//     } else {
+//       message.channel.send("Error: You must be in a voice channel.");
+//     }
+//   }
+// };
