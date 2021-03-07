58b8d3ae74f087e9034bc5785bf7f1d12063f143
Eric Kong <eric.kong98@gmail.com>
2021-03-06 20:13:31 -0500

    quick fix

--- commands/newtype.js
+++ commands/newtype.js
@@ -10,7 +10,7 @@ module.exports = {
   usage: "!type [name]",
   cooldown: 5,
   execute(message, args) {
-    const AllTypes = Types.getAllTypes();
+    const AllTypes = Types.getAllTypes;
     console.log(AllTypes);
     // message.reply((args[0] || "Bryant") + " the type of guy to " + MESSAGES[Math.floor(Math.random() * MESSAGES.length)])
   }
