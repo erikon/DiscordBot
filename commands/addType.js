545e6f7cd6c9a8e27d07ab7ecd21b38540127c4b
Eric Kong <eric.kong98@gmail.com>
2021-03-06 21:09:56 -0500

    persmissions for addtype

--- commands/addType.js
+++ commands/addType.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 
+const DEV_ROLE_ID = process.env.DEV_ROLE_ID;
+
 const { Types } = require("../common/dbObjects");
 
 
@@ -10,7 +12,13 @@ module.exports = {
   usage: "!type [name]",
   cooldown: 5,
   execute(message, args) {
-    // const addType = Types.create({message: args})
-    console.log(args);
+    if (message.member.roles.cache.some(role => role.id === DEV_ROLE_ID)) {
+      console.log('You are JuanBotDev');
+    } else {
+      console.log('You are not JuanBotDev');
+    }
+
+
+    // const addType = Types.create({message: args[0]})
   }
 };
