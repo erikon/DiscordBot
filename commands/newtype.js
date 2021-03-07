401271726545790c41512a60d56ddd791a861c8e
Eric Kong <eric.kong98@gmail.com>
2021-03-06 20:20:36 -0500

    test

--- commands/newtype.js
+++ commands/newtype.js
@@ -10,7 +10,10 @@ module.exports = {
   usage: "!type [name]",
   cooldown: 5,
   execute(message, args) {
-    const AllTypes = Types.getAllTypes;
+    const AllTypes = Types.findAll().then(types => {
+        console.log(types);
+        console.log(types.length);
+    });
     console.log(AllTypes);
     // message.reply((args[0] || "Bryant") + " the type of guy to " + MESSAGES[Math.floor(Math.random() * MESSAGES.length)])
   }
