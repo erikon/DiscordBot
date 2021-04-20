30ec94acf83a88dcefae5c13f9d7a57bfb937ad8
Eric Kong <eric.kong98@gmail.com>
2021-04-19 22:05:34 -0400

    fix

---
+++ commands/future.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: "future",
+  description: "Time for the Future",
+  usage: "(Must be in a voice channel)",
+  cooldown: 5,
+  async execute(message, args) {
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play("./assets/future.mp3");
+      dispatcher.setVolume(0.8);
+      dispatcher.on("start", () => {
+        console.log("Audio is now playing!");
+      });
+
+      dispatcher.on("finish", () => {
+        console.log("Audio has finished playing!");
+        dispatcher.destroy();
+        connection.disconnect();
+      });
+
+      // Always remember to handle errors appropriately!
+      dispatcher.on("error", err => {
+        console.log(err);
+        dispatcher.destroy();
+        connection.disconnect();
+      });
+    }
+  }
+};
